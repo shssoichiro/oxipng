bb2aa4ce9b0fc18af8070efa55aca5e80fd18734
Kornel Lesiński <kornel@geekhood.net>
2018-07-16 15:26:30 +0100

    Avoid slow modulo

--- src/reduction/alpha.rs
+++ src/reduction/alpha.rs
@@ -1,12 +1,14 @@
 use png::PngData;
 
-pub fn reduce_alpha_channel(png: &mut PngData, channels: usize) -> Option<Vec<u8>> {
-    let byte_depth: u8 = png.ihdr_data.bit_depth.as_u8() >> 3;
-    let bpp: usize = channels * byte_depth as usize;
-    let colored_bytes = bpp - byte_depth as usize;
+pub fn reduce_alpha_channel(png: &mut PngData, channels: u8) -> Option<Vec<u8>> {
+    let byte_depth = png.ihdr_data.bit_depth.as_u8() >> 3;
+    let bpp = channels * byte_depth;
+    let bpp_mask = bpp - 1;
+    assert_eq!(0, bpp & bpp_mask);
+    let colored_bytes = bpp - byte_depth;
     for line in png.scan_lines() {
         for (i, &byte) in line.data.iter().enumerate() {
-            if i % bpp >= colored_bytes {
+            if i as u8 & bpp_mask >= colored_bytes {
                 if byte != 255 {
                     return None;
                 }
@@ -18,7 +20,7 @@ pub fn reduce_alpha_channel(png: &mut PngData, channels: usize) -> Option<Vec<u8
     for line in png.scan_lines() {
         reduced.push(line.filter);
         for (i, &byte) in line.data.iter().enumerate() {
-            if i % bpp >= colored_bytes {
+            if i as u8 & bpp_mask >= colored_bytes {
                 continue;
             } else {
                 reduced.push(byte);
@@ -29,7 +31,7 @@ pub fn reduce_alpha_channel(png: &mut PngData, channels: usize) -> Option<Vec<u8
     // sBIT contains information about alpha channel's original depth,
     // and alpha has just been removed
     if let Some(sbit_header) = png.aux_headers.get_mut(&"sBIT".to_string()) {
-        assert_eq!(sbit_header.len(), channels);
+        assert_eq!(sbit_header.len(), channels as usize);
         sbit_header.pop();
     }
 
