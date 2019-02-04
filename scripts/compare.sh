d98fe70bcf45de97a685ca2a56aa6e1ec9d56a6e
Josh Holmer <jholmer.in@gmail.com>
2019-02-04 10:01:50 -0500

    Update compare.sh

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -8,7 +8,9 @@ OXIPNG_VERSION=$(./target/release/oxipng -V)
 OPTIPNG_VERSION=$(optipng -v | head -n 1)
 RUST_VERSION=$(rustc -V)
 echo "Tested $OXIPNG_VERSION (compiled on $RUST_VERSION) against $OPTIPNG_VERSION on $CPU with $CORES logical cores" >> README.md
-echo -e '\n\n' >> README.md
+echo -e '\n\n```\n' >> README.md
 
 hyperfine --warmup 5 './target/release/oxipng -P ./tests/files/rgb_16_should_be_grayscale_8.png' 'optipng -simulate ./tests/files/rgb_16_should_be_grayscale_8.png' | ./node_modules/.bin/strip-ansi >> README.md
 hyperfine --warmup 5 './target/release/oxipng -o4 -P ./tests/files/rgb_16_should_be_grayscale_8.png' 'optipng -o 4 -simulate ./tests/files/rgb_16_should_be_grayscale_8.png' | ./node_modules/.bin/strip-ansi >> README.md
+
+echo -e '\n```\n' >> README.md
