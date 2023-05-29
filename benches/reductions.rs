9dcc819e70a04d3f746d4fdde7279a0e92c39e31
Andrew <andrew@digerati.co.nz>
2023-05-30 08:16:33 +1200

    Prevent conversion from grayscale when disabled

--- benches/reductions.rs
+++ benches/reductions.rs
@@ -229,7 +229,7 @@ fn reductions_rgba_to_palette_8(b: &mut Bencher) {
     let input = test::black_box(PathBuf::from("tests/files/rgba_8_should_be_palette_8.png"));
     let png = PngData::new(&input, &Options::default()).unwrap();
 
-    b.iter(|| color::reduced_to_indexed(&png.raw));
+    b.iter(|| color::reduced_to_indexed(&png.raw, true));
 }
 
 #[bench]
@@ -237,7 +237,7 @@ fn reductions_rgb_to_palette_8(b: &mut Bencher) {
     let input = test::black_box(PathBuf::from("tests/files/rgb_8_should_be_palette_8.png"));
     let png = PngData::new(&input, &Options::default()).unwrap();
 
-    b.iter(|| color::reduced_to_indexed(&png.raw));
+    b.iter(|| color::reduced_to_indexed(&png.raw, true));
 }
 
 #[bench]
@@ -247,7 +247,7 @@ fn reductions_grayscale_8_to_palette_8(b: &mut Bencher) {
     ));
     let png = PngData::new(&input, &Options::default()).unwrap();
 
-    b.iter(|| color::reduced_to_indexed(&png.raw));
+    b.iter(|| color::reduced_to_indexed(&png.raw, true));
 }
 
 #[bench]
