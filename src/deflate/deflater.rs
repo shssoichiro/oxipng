0111c3a01680deeedfd04b48884d7a1e242e7947
Andrew <andrew@digerati.co.nz>
2023-09-19 20:49:45 +1200

    Remove libdeflate slack space no longer needed

--- src/deflate/deflater.rs
+++ src/deflate/deflater.rs
@@ -4,16 +4,14 @@ use libdeflater::*;
 
 pub fn deflate(data: &[u8], level: u8, max_size: &AtomicMin) -> PngResult<Vec<u8>> {
     let mut compressor = Compressor::new(CompressionLvl::new(level.into()).unwrap());
-    // If adhering to a max_size we need to include at least 9 extra bytes of slack space (as specified in docs).
     let capacity = max_size
         .get()
-        .unwrap_or_else(|| compressor.zlib_compress_bound(data.len()))
-        + 9;
+        .unwrap_or_else(|| compressor.zlib_compress_bound(data.len()));
     let mut dest = vec![0; capacity];
     let len = compressor
         .zlib_compress(data, &mut dest)
         .map_err(|err| match err {
-            CompressionError::InsufficientSpace => PngError::DeflatedDataTooLong(capacity - 9),
+            CompressionError::InsufficientSpace => PngError::DeflatedDataTooLong(capacity),
         })?;
     dest.truncate(len);
     Ok(dest)
