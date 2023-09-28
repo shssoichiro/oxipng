f66c660b892a3b8a63fc729f8858a1ea3808b93a
Alejandro González <me@alegon.dev>
2023-09-28 13:22:54 +0200

    Update `zopfli` to v0.8.0
    This update brings several parameterization and usage flexibility
    improvements on Zopfli, allowing users to choose to limit execution time
    by a number of iterations without improvement, and exposing a more
    advanced `ZlibEncoder` struct to tune compression block sizes and
    DEFLATE block types. Some minor microoptimizations were also made.
    
    For now, I don't expect this PR to substantially affect how OxiPNG
    compresses images using its Zopfli mode, but the additional parameter
    customization may come in handy for future work improving how Zopfli is
    used in OxiPNG.

--- src/deflate/zopfli_oxipng.rs
+++ src/deflate/zopfli_oxipng.rs
@@ -6,10 +6,10 @@ pub fn deflate(data: &[u8], iterations: NonZeroU8) -> PngResult<Vec<u8>> {
 
     let mut output = Vec::with_capacity(max(1024, data.len() / 20));
     let options = zopfli::Options {
-        iteration_count: iterations,
+        iteration_count: iterations.into(),
         ..Default::default()
     };
-    match zopfli::compress(&options, &zopfli::Format::Zlib, data, &mut output) {
+    match zopfli::compress(options, zopfli::Format::Zlib, data, &mut output) {
         Ok(_) => (),
         Err(_) => return Err(PngError::new("Failed to compress in zopfli")),
     };
