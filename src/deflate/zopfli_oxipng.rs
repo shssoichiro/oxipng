86ff1e5833b2bc326dc44e7fcd9fda1596fc5a60
Andrew <andrew@digerati.co.nz>
2024-02-20 16:41:29 +1300

    Workaround weird regression with &[u8] and zopfli

--- src/deflate/zopfli_oxipng.rs
+++ src/deflate/zopfli_oxipng.rs
@@ -8,7 +8,9 @@ pub fn deflate(data: &[u8], iterations: NonZeroU8) -> PngResult<Vec<u8>> {
         iteration_count: iterations.into(),
         ..Default::default()
     };
-    match zopfli::compress(options, zopfli::Format::Zlib, data, &mut output) {
+    // Since Rust v1.74, passing &[u8] directly into zopfli causes a regression in compressed size
+    // for some files. Wrapping the slice in another Read implementer such as Box fixes it for now.
+    match zopfli::compress(options, zopfli::Format::Zlib, Box::new(data), &mut output) {
         Ok(_) => (),
         Err(_) => return Err(PngError::new("Failed to compress in zopfli")),
     };
