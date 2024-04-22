1ad90b389ffb426a21e4a4d64f614f62ae72a443
Andrew <andrew@digerati.co.nz>
2024-04-23 08:58:32 +1200

    Create assets dir relative to OUT_DIR

--- build.rs
+++ build.rs
@@ -24,8 +24,14 @@ fn main() -> Result<(), Error> {
     println!("cargo:rerun-if-changed=src/cli.rs");
     println!("cargo:rerun-if-changed=src/display_chunks.rs");
 
-    // Create `generated/assets/` folder.
-    let path = env::current_dir()?.join("generated").join("assets");
+    // Create `target/<debug|release>/assets/` folder.
+    let outdir = match env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(outdir) => outdir,
+    };
+    let out_path = PathBuf::from(outdir);
+    let mut path = out_path.ancestors().nth(3).unwrap().to_owned();
+    path.push("assets");
     std::fs::create_dir_all(&path).unwrap();
 
     build_manpages(&path)?;
